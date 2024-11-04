40e91496eedcefdfc663195828d2336b12b41cc8
MohammadAliAl <104681553+MohammadAliAl@users.noreply.github.com>
2024-11-04 15:16:58 +0530

    Add files via upload

---
+++ register.js
@@ -0,0 +1,31 @@
+  // Import the functions you need from the SDKs you need
+
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+
+  // TODO: Add SDKs for Firebase products that you want to use
+
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+
+  // Your web app's Firebase configuration
+
+  const firebaseConfig = {
+
+    apiKey: "AIzaSyCNWW_7rPnCLIqDC0ObcuhZnrn8F6MeGHs",
+
+    authDomain: "nexus-knowledge.firebaseapp.com",
+
+    projectId: "nexus-knowledge",
+
+    storageBucket: "nexus-knowledge.firebasestorage.app",
+
+    messagingSenderId: "27095795050",
+
+    appId: "1:27095795050:web:c05eb65cf88ea52a371113"
+
+  };
+
+
+  // Initialize Firebase
+
+  const app = initializeApp(firebaseConfig);
